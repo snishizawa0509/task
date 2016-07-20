7452c4ceed63fca640bcc435e1fb2d89469824d1
snishizawa0509 <s-nishizawa@towatech.net>
2016-07-20 19:14:50 +0900

    validation added

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,6 @@
 class Note < ApplicationRecord
   belongs_to :category
     validates :title, presence: true
- # validates :user_id, presence: true
+    validates :category_id, presence:true
+    # validates :user_id, presence: true
 end
