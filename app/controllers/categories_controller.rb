43360f50aa6abd28b506dab8fd3bfef751c6cc10
snishizawa0509 <s-nishizawa@towatech.net>
2016-07-20 18:02:36 +0900

    helper controller routes category=>categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,4 @@
-class CategoryController < ApplicationController
+class CategoriesController < ApplicationController
 
   def new
     @categories = Category.all 
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
 	patch '/notes/:id' => 'notes#update', as:'note_update'
   get '/notes/:id/edit' => 'notes#edit', as:'note_edit'
 	delete '/notes:id' => 'notes#destroy', as:'note_destroy'
-  get '/category' => 'category#new', as:'category'
-  post '/category' => 'category#create', as:'category_create'
-  delete '/category:id' => 'category#destroy', as:'category_destroy'
+  get '/category' => 'categories#new', as:'category'
+  post '/category' => 'categories#create', as:'category_create'
+  delete '/category:id' => 'categories#destroy', as:'category_destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
