f619110f965c45da8d451f29c2da65ee6d0725e8
snishizawa0509 <s-nishizawa@towatech.net>
2016-07-20 16:34:34 +0900

    category_delete_error fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160720023647) do
+ActiveRecord::Schema.define(version: 20160720053107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,10 +22,11 @@ ActiveRecord::Schema.define(version: 20160720023647) do
   end
 
   create_table "notes", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "title"
     t.text     "content"
+    t.integer  "category_id"
   end
 
   create_table "users", force: :cascade do |t|
