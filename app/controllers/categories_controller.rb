29f47cde1ba1ad7a648d50b8a304024360dcd994
snishizawa0509 <s-nishizawa@towatech.net>
2016-07-21 09:45:23 +0900

    added editpage edit update

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,11 +8,25 @@ class CategoriesController < ApplicationController
   def create
     @category = Category.new
     @category.name = params[:name]
-    @category.save
     if @category.save
       redirect_to categories_path, notice: '追加しました'
     else
       redirect_to categories_path
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    @category.name = params[:name]
+    if @category.save
+      redirect_to categories_path, notice: '編集しました'
+    else
+      render :edit 
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :categories , only: [:index, :create, :destroy]
+  resources :categories , only: [:edit, :update, :index, :create, :destroy]
   resources :notes
 
   
