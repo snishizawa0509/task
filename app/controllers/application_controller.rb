ef930854a3b11ef99c8f9920211657ee3fbfd801
snishizawa0509 <s-nishizawa@towatech.net>
2016-07-22 09:27:33 +0900

    add sign_up application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
 class ApplicationController < ActionController::Base    
- # before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  #private
-   # def configure_permitted_parameters
-    #  devise_parameter_sanitizer.for(:sign_up) << :name
-   # end 
+                                                     protected
+
+                                                     def configure_permitted_parameters
+                                                         devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+                                                     end
+  
+
 end
