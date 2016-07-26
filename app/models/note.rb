dd7ab9f1b0c5ea4285abcde489456c392c4188a9
snishizawa0509 <s-nishizawa@towatech.net>
2016-07-26 12:52:58 +0900

    notes_model add mount_uploader :image, ImageUploader

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,5 +3,5 @@ class Note < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
   validates :category_id, presence:true
-    # validates :user_id, presence: true
+  mount_uploader :image, ImageUploader
 end
