8b5b12f4b18d7161ba2443b2c51d06be279583b3
snishizawa0509 <s-nishizawa@towatech.net>
2016-07-27 19:02:40 +0900

    test login&create_note

---
+++ test/integration/note_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class NoteTest < ActionDispatch::IntegrationTest
+  test "Create a new post" do
+    login_as(users(:user1))
+    visit notes_path
+    click_link '新規投稿'
+    fill_in('note[title]', with: '記事')
+    fill_in('note[content]', with: 'こんにちわ')
+    select('テスト1', :from => 'note[category_id]')
+    assert_difference 'Note.count', 1, '投稿が作成されるべき' do
+      click_button '保存'
+    end
+
+    assert page.has_content?('投稿されました')
+    note = Note.last
+    assert_equal '記事', note.title
+    assert_equal 'こんにちわ', note.content
+    assert_equal 'テスト1', note.category.name
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,10 +8,19 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+module LoginHelper
+  def login_as(user)
+    visit new_user_session_path
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: 'password'
+    click_button 'ログイン'
+  end
+end
+
 class ActionDispatch::IntegrationTest
   #  Make the Capybara DSL available in all integration tests
   include Capybara::DSL
-
+  include LoginHelper
   # Reset sessions and driver between tests
   # Use super wherever this method is redefined in your individual test classes
   def teardown
