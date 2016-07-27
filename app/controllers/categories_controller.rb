45a10fff116d40aab2a83e8f304b1959d9d9c30b
snishizawa0509 <s-nishizawa@towatech.net>
2016-07-27 14:51:08 +0900

    change form_for

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,16 +1,14 @@
 class CategoriesController < ApplicationController
   before_action :authenticate_user!
-  before_action :correct_user, only: [:edit, :update ,:destroy]
+  before_action :correct_user, only: [:edit, :update , :destroy]
+  before_action :set_category, only: [:edit, :update , :destroy]
   def index
     @categories = Category.where(user: current_user) 
     @category = Category.new
   end
 
   def create
-    @category = Category.new
-    @category.name = params[:name]
-    @category.user = current_user
-    
+    @category = current_user.categories.build(category_params) 
     if @category.save
       redirect_to categories_path, notice: '追加しました'
     else
@@ -19,13 +17,10 @@ class CategoriesController < ApplicationController
   end
 
   def edit
-    @category = Category.find(params[:id])
   end
 
   def update
-    @category = Category.find(params[:id])
-    @category.name = params[:name]
-    if @category.save
+    if @category.update(category_params)
       redirect_to categories_path, notice: '編集しました'
     else
       render :edit 
@@ -33,12 +28,19 @@ class CategoriesController < ApplicationController
   end
 
   def destroy
-    @category = Category.find(params[:id])
     @category.destroy
     redirect_to categories_path
   end
    
-  private
+  private 
+    def set_category
+        @category = Category.find(params[:id])
+    end
+
+    def category_params
+        params.require(:category).permit(:name)
+    end
+  
     def correct_user
       category = Category.find(params[:id])
       if current_user.id != category.user_id
