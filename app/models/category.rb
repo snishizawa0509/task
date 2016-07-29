8672e07aae0a22e7f92fcba5ca51db55b848f552
snishizawa0509 <s-nishizawa@towatech.net>
2016-07-29 11:16:34 +0900

    edit home, delete forgetpass_link

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,5 @@ class Category < ApplicationRecord
   has_many :notes
   belongs_to :user
   validates :name, presence: true
+  validates :name, length: { maximum: 20 }
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -2,6 +2,7 @@ class Note < ApplicationRecord
   belongs_to :category
   belongs_to :user
   validates :title, presence: true
-  validates :category_id, presence:true
+  validates :category_id, presence:true 
+  validates :title, length: { maximum: 15 }
   mount_uploader :image, ImageUploader
 end
