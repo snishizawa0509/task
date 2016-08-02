3f7a3fbc0ee0a6079aa73db6ba68bed32eac6fc7
snishizawa0509 <s-nishizawa@towatech.net>
2016-08-02 16:04:17 +0900

    fix devise-view

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,8 +1,7 @@
 class Note < ApplicationRecord
   belongs_to :category
   belongs_to :user
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 15 }
   validates :category_id, presence: true
-  validates :title, length: { maximum: 15 }
   mount_uploader :image, ImageUploader
 end
