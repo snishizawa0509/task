89d9a410b2136b8b1ce185fc1129070ce9617822
snishizawa0509 <s-nishizawa@towatech.net>
2016-08-02 16:24:20 +0900

    unique category name on a per-user

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,5 @@ class Category < ApplicationRecord
   belongs_to :user
   validates :name, presence: true
   validates :name, length: { maximum: 20 }
+  validates :name, uniqueness: { scope: [:user_id] }
 end
